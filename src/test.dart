06c717d2b153c232e0ebc99c4669e4ae414d3b5d
Max Fechner <max.fechner@gmail.com>
2020-09-04 16:01:56 +0200

    folder changes

--- src/test.dart
+++ src/test.dart
@@ -8,17 +8,17 @@ String minimalJSON(JSON) => jsonEncode(jsonDecode(JSON));
 
 testPharmacon({String language, String name}) {
   test('$language $name', () async {
-    String test = File('./test/$language.$name.json').readAsStringSync();
+    String test = File('../test/$language.$name.json').readAsStringSync();
     Pharmacon wiki = await loadPharmacon(language: language, name: name);
     expect(minimalJSON(test), equals(minimalJSON(wiki.JSON)));
   });
 }
 
 void main() {
-  for (File file in Directory('test').listSync()) {
+  for (File file in Directory('../test').listSync()) {
     print(file);
     List<String> parts =
-        file.path.replaceAll('test/', '').replaceAll('.json', '').split('.');
+        file.path.replaceAll('../test/', '').replaceAll('.json', '').split('.');
     // print(parts);
     testPharmacon(language: parts[0], name: parts[1]);
   }
